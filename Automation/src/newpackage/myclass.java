faddbff3fb269f538b22ae74ad29b8cd3d9bc183
Tauseef Ahmed <Tauseef Ahmed@HUSS9364>
2022-01-26 18:14:45 +0500

    commit and push

---
+++ Automation/src/newpackage/myclass.java
@@ -0,0 +1,24 @@
+package newpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class myclass {
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello world..!!!");
+//		System.setProperty("webdriver.chrome.driver", "C:\\Webdriver\\bin2\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		String baseUrl = "http://192.168.16.219:4510/";
+//		String tagName = "";
+//		driver.get(baseUrl);
+//		//tagName = driver.findElement(By.id("email")).getTagName();
+//		System.out.println(tagName);
+//		driver.close();
+//		System.exit(0);
+	}
+
+	}
+	
